3f9503a518ff7bfd2b46e7b8ddf3f54b169ee331
akira289 <a.fukazawa1@gmail.com>
2019-12-07 15:31:01 +0900

    Implement add member function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,6 +12,19 @@ $(function(){
               </div>`;
     $("#user-search-result").append(html);
   }
+  function addSelectUser(user_id, user_name){
+    let html =  `<div class="chat-group-user clearfix" id="${user_id}">
+                  <p class="chat-group-user__name">${user_name}</p>
+                  <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${user_id}" data-user-name="${user_name}">削除</div>
+                </div>`;
+    $("#chat-group-users").append(html);
+  }
+  function addMember(user_id){
+    let html =  `<input name='group[user_ids][]' type='hidden' value='${user_id}'>
+    `
+    $(`#${user_id}`).append(html);
+
+  }
   $("#user-search-field").on("keyup", function(){
     let input = $("#user-search-field").val();
     $.ajax({
@@ -35,5 +48,12 @@ $(function(){
     .fail(function(){
       alert("通信エラーです。ユーザーを表示できません");
     })
+  });
+  $("#user-search-result").on('click', ".user-search-add", function(){
+    let user_id = $(this).data("user-id");
+    let user_name = $(this).data("user-name");
+    $(this).parent().remove();
+    addSelectUser(user_id, user_name);
+    addMember(user_id);
   });
 });
